e86fc5cb37d76a61b47642e289788f9f2052b99a
M.D.Gregory <matthewdarylgregory.com@gmail.com>
2025-07-31 19:39:44 +0100

    added animations do player and ghost

--- player_ghost.gd
+++ player_ghost.gd
@@ -4,11 +4,15 @@ var movement_data : Dictionary = Dictionary()
 var count = -1
 @onready var loop = get_node("../Level/Looping_Componenet")
 @onready var loop_position_end: Vector2 = Vector2(loop.br_x*64, loop.br_y*64)
+@onready var ghost_sprite: AnimatedSprite2D = %GhostSprite
 
 func _ready() -> void:
-	print(get_node(".."))
 	if movement_data.has(0):
-		global_position = movement_data[0]
+		var frame_data = movement_data[0]
+		position = Vector2(frame_data["position_x"], frame_data["position_y"])
+		if ghost_sprite:
+			ghost_sprite.play(frame_data["animation"])
+			ghost_sprite.flip_h = frame_data["flip_h"]
 
 func _physics_process(_delta: float) -> void:
 	update_position()
@@ -18,4 +22,8 @@ func _physics_process(_delta: float) -> void:
 func update_position():
 	count += 1
 	if movement_data.has(count):
-		global_position = movement_data[count]
+		var frame_data = movement_data[count]
+		position = Vector2(frame_data["position_x"], frame_data["position_y"])
+		if ghost_sprite:
+			ghost_sprite.play(frame_data["animation"])
+			ghost_sprite.flip_h = frame_data["flip_h"]
