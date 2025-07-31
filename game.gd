22c8022d6d84d2ba27763327d1ab59b9de28fc92
ZBC95 <zoebcameron@outlook.com>
2025-07-31 18:30:09 +0100

    ghost updates

--- game.gd
+++ game.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
 @export var ghost_scene : PackedScene
+@onready var player = get_node("Player")
+@onready var loop = get_node("Level/Looping_Componenet")
 
 func _ready():
 	SignalBus.player_looped.connect(_on_player_looped)
+	var ghost_init = ghost_scene.instantiate()
+	ghost_init.is_static = true
+	ghost_init.movement_data = {0: player.position + (Vector2(loop.br_x*64, loop.br_y*64)-Vector2(loop.bl_x*64, loop.bl_y*64))}
+	add_child(ghost_init)
 
 func _on_player_looped(movement_data):
 	var ghost = ghost_scene.instantiate()
 	ghost.movement_data = movement_data
+	var ghost2 = ghost_scene.instantiate()
+	ghost2.movement_data = {0: (player.position)}
+	ghost2.is_static = true
+	print(ghost.movement_data)
+	print(ghost2.movement_data)
 	add_child(ghost)
+	add_child(ghost2)
