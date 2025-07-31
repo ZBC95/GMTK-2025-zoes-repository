217b242b541b5c40e2d550f7aeaa65c7f2db1c05
ZBC95 <zoebcameron@outlook.com>
2025-07-31 22:54:58 +0100

    update1

--- camera_2d.gd
+++ camera_2d.gd
@@ -4,6 +4,14 @@ extends Camera2D
 @onready var loop = get_node("../Level/Looping_Componenet")
 
 func _ready() -> void:
+	SignalBus.player_looped.connect(_on_player_looped)
 	limit_left = loop.bl_x*64
 	limit_top = loop.tl_y*64
 	limit_bottom = loop.bl_y*64
+
+
+func _physics_process(delta: float) -> void:
+	position = player.position
+
+func _on_player_looped(movement_data):
+	limit_left = -1000000
