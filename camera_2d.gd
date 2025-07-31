c6f3d25e4db4d9e26bafd7e9405a5bcd57994871
ZBC95 <zoebcameron@outlook.com>
2025-08-01 00:19:18 +0100

    camera change

--- camera_2d.gd
+++ camera_2d.gd
@@ -6,12 +6,22 @@ extends Camera2D
 func _ready() -> void:
 	SignalBus.player_looped.connect(_on_player_looped)
 	limit_left = loop.bl_x*64
-	limit_top = loop.tl_y*64
-	limit_bottom = loop.bl_y*64
+	position_camera()
+
 
 
 func _physics_process(delta: float) -> void:
-	position = player.position
+	position_camera()
+
 
 func _on_player_looped(movement_data):
 	limit_left = -1000000
+
+func position_camera():
+	if get_viewport_rect().size.y < loop.bl_y*64 - loop.tl_y*64:
+		limit_top = loop.tl_y*64
+		limit_bottom = loop.bl_y*64
+		position = player.position
+	else:
+		position.y = (loop.bl_y*64 + loop.tl_y*64)/2
+		position.x = player.position.x
