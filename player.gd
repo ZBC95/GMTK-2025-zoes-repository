b797540311c7496049d74c709d49e08a2d2f6a7d
M.D.Gregory <matthewdarylgregory.com@gmail.com>
2025-08-01 11:51:25 +0100

    Added FSM and implemented blocking mechanic

--- player.gd
+++ player.gd
@@ -4,27 +4,31 @@ extends CharacterBody2D
 @export var JUMP_VELOCITY = 600.0
 @export var gravity = 1000
 
+enum PlayerState {
+	IDLE,
+	RUN,
+	JUMP_UP,
+	FALL,
+	LAND,
+	SKID,
+	BLOCK
+}
+
+var current_state: PlayerState = PlayerState.IDLE
+var frame_counter: int = 0
+var was_on_floor: bool = true
+var previous_direction: float = 0.0
+var is_blocking: bool = false
+var lap_start_count = 0  # Track when current lap started
+var count = 0
+# A Dictionary to store our position
+var movement_data = {}
+
 @onready var loop = get_node("../Level/Looping_Componenet")
 @onready var loop_position_start: Vector2 = Vector2(loop.bl_x*64, loop.bl_y*64)
 @onready var loop_position_end: Vector2 = Vector2(loop.br_x*64, loop.br_y*64)
 @onready var player_sprite: AnimatedSprite2D = %PlayerSprite
 
-
-var count = 0
-
-# A Dictionary to store our position
-var movement_data = {}
-
-func _ready():
-	#Ititial position
-	movement_data[0] = {
-		"position_x": position.x,
-		"position_y": position.y,
-		"animation": player_sprite.animation,
-		"flip_h": player_sprite.flip_h
-	}
-
-var lap_start_count = 0  # Track when current lap started
 
 func _physics_process(delta: float) -> void:
 	record_movement()
@@ -32,37 +36,103 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		# Set jump_up or fall animation based on vertical velocity
+		if velocity.y < 0:
+			current_state = PlayerState.JUMP_UP
+		else:
+			current_state = PlayerState.FALL
 
 	# Handle jump.
-	if Input.is_action_pressed("Jump") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = -JUMP_VELOCITY
+		current_state = PlayerState.JUMP_UP
+
+	# Handle blocking
+	if Input.is_action_pressed("ui_down") and is_on_floor():
+		is_blocking = true
+		current_state = PlayerState.BLOCK
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	elif Input.is_action_just_released("ui_down"):
+		is_blocking = false
+		current_state = PlayerState.IDLE
 	
+# Only allow movement if not blocking
+	if !is_blocking:
+		var direction := Input.get_axis("Left", "Right")
+		
+		if direction:
+			# Check for skid state
+			if abs(velocity.x) > SPEED * 0.8 and sign(direction) != sign(previous_direction) and previous_direction != 0:
+				current_state = PlayerState.SKID
+				player_sprite.frame = 0  # Reset frame when entering skid state
+			
+			velocity.x = direction * SPEED
+			if direction == -1:
+				player_sprite.flip_h = true
+			else:
+				player_sprite.flip_h = false
+				
+			if current_state != PlayerState.SKID and is_on_floor():
+				current_state = PlayerState.RUN
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			if current_state != PlayerState.LAND and is_on_floor() and !is_blocking:
+				current_state = PlayerState.IDLE
+
+	# Landing state check
+	if is_on_floor() and !was_on_floor:
+		current_state = PlayerState.LAND
+		player_sprite.frame = 0  # Reset frame when landing
+
+	## Falling state check
+	#if !is_on_floor() and velocity.y > 0:
+		#current_state = PlayerState.FALL
+
+	update_animation()
+	
+	was_on_floor = is_on_floor()
+	previous_direction = sign(velocity.x)
+
 	# Handle looping off screen right
 	if position.x > loop_position_end.x:
-		# Create dictionary with just this lap's movement data
 		var lap_data = {}
 		for i in range(lap_start_count, count + 1):
 			if movement_data.has(i):
 				lap_data[i - lap_start_count] = movement_data[i]
-				
+		
 		SignalBus.player_looped.emit(lap_data)
 		position.x = loop_position_start.x
-		lap_start_count = count + 1  # Start counting new lap from here
-	
-	# Get the input direction and handle the movement/deceleration.
-	var direction := Input.get_axis("Left", "Right")
-	if direction:
-		player_sprite.play("run")
-		velocity.x = direction * SPEED
-		if direction == -1:
-			player_sprite.flip_h = true
-		else:
-			player_sprite.flip_h = false
-	else:
-		player_sprite.play("idle")
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		lap_start_count = count + 1
 
 	move_and_slide()
+
+func update_animation():
+	match current_state:
+		PlayerState.IDLE:
+			player_sprite.play("idle")
+		PlayerState.RUN:
+			player_sprite.play("run")
+		PlayerState.JUMP_UP:
+			player_sprite.play("jump_up")
+		PlayerState.FALL:
+			player_sprite.play("fall")
+		PlayerState.LAND:
+			if player_sprite.animation != "land":
+				player_sprite.play("land")
+				player_sprite.frame = 0
+			# Wait for animation to complete
+			if player_sprite.frame >= player_sprite.sprite_frames.get_frame_count("land") - 1:
+				current_state = PlayerState.IDLE
+		PlayerState.SKID:
+			if player_sprite.animation != "skid":
+				player_sprite.play("skid")
+				player_sprite.frame = 0
+			# Wait for animation to complete
+			if player_sprite.frame >= player_sprite.sprite_frames.get_frame_count("skid") - 1:
+				current_state = PlayerState.RUN
+		PlayerState.BLOCK:
+			player_sprite.play("block")
+
 
 func record_movement():
 	count += 1
