680876767d873cb808d1e12b574b791ad870cf48
ZBC95 <zoebcameron@outlook.com>
2025-08-01 12:08:47 +0100

    Input control update

--- player.gd
+++ player.gd
@@ -48,11 +48,11 @@ func _physics_process(delta: float) -> void:
 		current_state = PlayerState.JUMP_UP
 
 	# Handle blocking
-	if Input.is_action_pressed("ui_down") and is_on_floor():
+	if Input.is_action_pressed("Down") and is_on_floor():
 		is_blocking = true
 		current_state = PlayerState.BLOCK
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	elif Input.is_action_just_released("ui_down"):
+	elif Input.is_action_just_released("Down"):
 		is_blocking = false
 		current_state = PlayerState.IDLE
 	
