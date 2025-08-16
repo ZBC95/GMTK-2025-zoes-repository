0cde49ee7ab8187f765d8734e42ca0fb64b1ffa4
ZBC95 <zoebcameron@outlook.com>
2025-08-16 21:41:53 +0100

    ghost flash

--- player_ghost.gd
+++ player_ghost.gd
@@ -7,6 +7,9 @@ var overlaps
 var channel_to_emit: int = 0
 var colours = [Color.BLUE,Color.BLUE, Color.RED,Color.RED, Color.GREEN,Color.GREEN, Color.ORANGE,Color.ORANGE, Color.YELLOW,Color.YELLOW, Color.WHITE,Color.WHITE, Color.PURPLE,Color.PURPLE,]
 var colour
+var found_unblock = false
+var unblock_forward = 0
+var next_unblock: int
 
 @onready var loop = get_node("../Level/Looping_Componenet")
 @onready var loop_position_end: Vector2 = Vector2(loop.br_x*64, loop.br_y*64)
@@ -54,9 +57,35 @@ func update_collision_layers():
 		$Area2D.set_collision_layer_value(3, false)
 		set_collision_layer_value(1, true)
 		set_collision_mask_value(1, true)
+		flash()
 	else:
 		ghost_sprite.modulate = colour
 		$Area2D.set_collision_layer_value(2, false)
 		$Area2D.set_collision_layer_value(3, true)
 		set_collision_layer_value(1, false)
 		set_collision_mask_value(1, false)
+		found_unblock = false
+
+func flash():
+	if found_unblock == false:
+		unblock_forward = 0
+		while found_unblock == false:
+			var frame_data = movement_data[count + unblock_forward]
+			if frame_data["animation"] != "block":
+				found_unblock = true
+				next_unblock = count + unblock_forward
+			else:
+				unblock_forward += 1
+			print(count)
+			print(unblock_forward)
+	if next_unblock - count < 30:
+		ghost_sprite.modulate.a = 0.4+(0.6*(float(next_unblock - count)/30.0))
+	elif next_unblock - count < 90:
+		if count%6 < 3:
+			ghost_sprite.modulate.a = 0.8
+	elif next_unblock - count < 150:
+		if count%16 < 4:
+			ghost_sprite.modulate.a = 0.8
+	elif next_unblock - count < 240:
+		if count%30 < 4:
+			ghost_sprite.modulate.a = 0.8
